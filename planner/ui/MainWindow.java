5fb6986ab1564bb11254fa2f6cd759071b68b3fb
hewmon <x>
2011-10-19 10:37:51 -0400

    Changing to cross-platform look and feel

--- planner/ui/MainWindow.java
+++ planner/ui/MainWindow.java
@@ -1,10 +1,22 @@
 package planner.ui;
 import javax.swing.*;
 public class MainWindow extends JFrame {
-   private static String NAME = "Computer use planning and monitoring";
-   public MainWindow() {
-       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       add(new TimeBlockTable());
-       pack();
-   }
+    private static String NAME = "Computer use planning and monitoring";
+    public MainWindow() {
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new TimeBlockTable());
+        pack();
+    }
 }
